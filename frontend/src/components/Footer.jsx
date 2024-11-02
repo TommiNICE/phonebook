e4d2be925364d29e01da905d58d112fca6802909
tomi <tomislav.varga@icloud.com>
2024-11-02 09:42:25 +0100

    Edited footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='my-4 text-white'>
             <a target="_blank" href="https://icons8.com/icon/26015/rotary-dial-telephone">Rotary Dial Telephone</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+            <p>© {new Date().getFullYear()} MIT License</p>
         </footer>
     )
 }
